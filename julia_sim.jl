2ce55288b23f04b74d06d674e9a7e5bdfcbfd99f
moiLaird <10575266+moiLaird@users.noreply.github.com>
2026-05-23 05:50:00 +0800

    Use Xoshiro256** PRNG for sampling

--- julia_sim.jl
+++ julia_sim.jl
@@ -4,12 +4,23 @@ using Base.Threads
     ((x^2 + 2.25y^2 + z^2 - 1.0)^3 - (x^2 + 0.1125y^2) * z^3) < 0
 end
 
-@inline function next_rand(s::UInt64)
-    s += 0x9e3779b97f4a7c15
-    z = xor(s, s >> 30) * 0xbf58476d1ce4e5b9
-    z = xor(z, z >> 27) * 0x94d049bb133111eb
-    u01 = Float64(xor(z, z >> 31) >> 11) * 2.0^-53
-    return u01, s
+function init_rng(seed::UInt64)
+    ntuple(4) do _
+        seed += 0x9e3779b97f4a7c15
+        z = xor(seed, seed >> 30) * 0xbf58476d1ce4e5b9
+        z = xor(z, z >> 27) * 0x94d049bb133111eb
+        xor(z, z >> 31)
+    end
+end
+# Xoshiro256** PRNG
+@inline function next_rand((s1, s2, s3, s4)::NTuple{4, UInt64})
+    u01 = Float64((bitrotate(s2 * 5, 7) * 9) >> 11) * 2.0^-53
+    return u01, (
+        xor(s1, s2, s4),
+        xor(s1, s2, s3),
+        xor(s1, s3, s2 << 17),
+        bitrotate(xor(s2, s4), 45)
+    )
 end
 
 function mc_vol(n, nthr = Threads.nthreads())
@@ -17,7 +28,7 @@ function mc_vol(n, nthr = Threads.nthreads())
     hits = zeros(Int64, nthr)
     @fastmath Threads.@threads for tid in 1:nthr
         samples = chunk + (tid <= remainder ? 1 : 0)
-        state, count = UInt64(12345 + tid * 31337), 0
+        state, count = init_rng(UInt64(12345 + tid * 31337)), 0
         for _ in 1:samples
             ux, state = next_rand(state)
             uy, state = next_rand(state)
